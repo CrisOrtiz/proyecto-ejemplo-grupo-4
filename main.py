a56bc25d65d47056e42fbe7fbb5d2f816c01d385
Cristhian Ortiz <cristhianortizmercado@gmail.com>
2026-03-25 22:27:57 -0400

    G4 update main and add git ignore (#1)
    * G4 Add gitignore
    
    * G4 enhance menu and asign tasks in comments

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 from datautils import *
 
-def mostrar_menu():
+def show_menu():
     print("\n--- Menu de medidas fundamentales ---")
+    print("1. Normalización Min-Max")
+    print("2. Normalización Z-Score")
+    print("3. Mediana")
+    print("4. Desviación Estándar") 
+    print("5. Salir\n")
 
-def ejecutar():
-    tareas = []
+def run():
     data = [10, 25, 28, 40, 50]
     print("Datos:", data)
 
     while True:
-        mostrar_menu()
-        opcion = input("Selecciona una opción: ")
+        show_menu()
+        option = input("Selecciona una opción: ")
         
-        match opcion:
+        match option:
             case "1":
-                # Agregar tarea 1 - min_max
+                # Agregar tarea 1 - min_max(data) - Cristhian
                 print("Tarea 1")
             case "2":
-                # Agregar tarea 2 - z_score
+                # Agregar tarea 2 - z_score(data) - Michael
                 print("Tarea 2")
             case "3":
-                # Agregar tarea 3 - median
+                # Agregar tarea 3 - median(data) - Billy
                 print("Tarea 3")
             case "4":
-                # Agregar tarea 4 - std_dev
+                # Agregar tarea 4 - std_dev(data) - Danna
                 print("Tarea 4")
             case "5":
                 print("Saliendo del programa...")
@@ -32,4 +36,4 @@ def ejecutar():
                 print("Opción no válida. Intenta de nuevo.")
 
 if __name__ == "__main__":
-    ejecutar()
+    run()
