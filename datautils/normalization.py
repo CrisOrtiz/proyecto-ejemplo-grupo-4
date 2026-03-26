ef39e3368ab02c7c37f7a88933920c5976b6975c
michaellimpias-ciber <michael.limpias@gmail.com>
2026-03-25 23:16:04 -0400

    agregar el metod z_score

--- datautils/normalization.py
+++ datautils/normalization.py
@@ -4,6 +4,9 @@ def min_max(data):
  # Añadir implementación de normalización min-max
  print("Función min_max aún no implementada.")
 
+# Normalización z-score
 def z_score(data):
-    # Añadir implementación de normalización z-score
-    print("Función z_score aún no implementada.")
+    mean = sum(data) / len(data)
+    std_dev = (sum((x - mean) ** 2 for x in data) / len(data)) ** 0.5
+    normalized_data = [(x - mean) / std_dev for x in data]
+    return normalized_data
