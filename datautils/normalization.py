25b1aad1e7455b66cbb7e97ee7894e73cf56495c
Cristhian Ortiz <cristhianortizmercado@gmail.com>
2026-03-27 10:37:01 -0400

    Agregar metodo min max (#2)
    * G4 added method logic for min_max
    
    * G4 Modify option 1 to show the result of normalization min max

--- datautils/normalization.py
+++ datautils/normalization.py
@@ -1,8 +1,9 @@
 from .statistics import mean, std_dev
 
 def min_max(data):
- # Añadir implementación de normalización min-max
- print("Función min_max aún no implementada.")
+  min_val = min(data)
+  max_val = max(data)
+  return [(x - min_val) / (max_val - min_val) for x in data]
 
 # Normalización z-score
 def z_score(data):
